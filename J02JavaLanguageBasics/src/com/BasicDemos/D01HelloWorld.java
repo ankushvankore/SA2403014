adc51c73ce76fb282839e521f50b860abc04f0c7
Ankush <ankushvankore@gmail.com>
2024-05-08 22:00:33 +0530

    Date-08-05-2024

---
+++ J02JavaLanguageBasics/src/com/BasicDemos/D01HelloWorld.java
@@ -0,0 +1,11 @@
+package com.BasicDemos;
+
+public class D01HelloWorld {
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World!!");
+		System.out.print("Welcome to Java");
+		System.out.println("Good Evening!!!");
+		
+	}
+}
