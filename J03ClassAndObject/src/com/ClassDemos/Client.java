d7cc3ebbbf8ab9ef567b197c4ba3ee35f1aab9a6
Ankush <ankushvankore@gmail.com>
2024-05-16 22:35:53 +0530

    Date: 16-05-2024

--- J03ClassAndObject/src/com/ClassDemos/Client.java
+++ J03ClassAndObject/src/com/ClassDemos/Client.java
@@ -3,6 +3,8 @@ package com.ClassDemos;
 public class Client {
 
 	public static void main(String[] args) {
+		Employee.setCompany("Star Agile");
+		
 		Employee e1 = new Employee();
 		//Employee is class name or Data type & e1 is an object
 		e1.setEmpId(101);
@@ -15,6 +17,17 @@ public class Client {
 		
 		Employee e2 = new Employee();
 		e2.displayData();
+		
+		System.out.println("==================================");
+		
+		Employee e3 = new Employee(123, "Poonam", 50000, "Female");
+		e3.displayData();
+		
+		e1.displayData();
+		
+		Employee e4 = new Employee(234, "Bunty", 50000);
+		e4.displayData();
 	}
 
 }
+;
--- J03ClassAndObject/src/com/ClassDemos/Employee.java
+++ J03ClassAndObject/src/com/ClassDemos/Employee.java
@@ -5,20 +5,42 @@ public class Employee {
 	private String empName;
 	private double basicSalary;
 	private String gender;
+	private static String company;
 	
-	public Employee()
+	public static void setCompany(String c)
+	{
+		company = c;
+	}
+	
+	public Employee()				//Plain / Default / No Parameter Constructor
 	{
 		empId = 100;
 		empName = "Anu";
 		basicSalary = 50000;
 		gender = "Female";
 		
-		System.out.println("Constructor called!!!");
+		System.out.println("Default Constructor called!!!");
+	}
+	public Employee(int id, String nm, double bs, String gn)	//Parameterized Constructor
+	{
+		empId = id;
+		empName = nm;
+		basicSalary = bs;
+		gender = gn;
+		System.out.println("Parameterized Constructor Called!!!");
+	}
+	public Employee(int id, String nm, double bs)
+	{
+		empId = id;
+		empName = nm;
+		basicSalary = bs;
+		gender = "Male";
+		System.out.println("Second Parameterized Constructor Called!!!");
 	}
 	
 	public void setEmpId(int id)		//Mutator Function
 	{
-		empId = id;
+		this.empId = id;
 	}
 	public int getEmpId()				//Accessor Function
 	{
@@ -28,9 +50,10 @@ public class Employee {
 	public void displayData()			//Facilitator Function
 	{
 		System.out.println("Employee Id  : " + empId);
-		System.out.println("Employee Name: " + empName);
-		System.out.println("Basic Salary : " + basicSalary);
-		System.out.println("Gender       : " + gender);
+		System.out.println("Employee Name: " + this.empName);
+		System.out.println("Basic Salary : " + this.basicSalary);
+		System.out.println("Gender       : " + this.gender);
+		System.out.println("Company      : " + company);
 	}
 	public double calculateSalary()
 	{
